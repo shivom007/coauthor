843dab85e63aa115f77617632c1ecad873a7151c
shivom007 <shivmahto37@gmail.com>
2024-05-15 22:16:09 +0530

    changes

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -3,8 +3,8 @@ import Quill from "quill";
 import { io } from "socket.io-client";
 import "quill/dist/quill.snow.css";
 import { useParams } from "react-router-dom";
-
-const SAVE_INTERVAL_MS = 500;
+import video from "./task3.mp4";
+const SAVE_INTERVAL_MS = 1000 * 5;
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -29,9 +29,9 @@ const TextEditor = () => {
   const [quill, setQuill] = useState(null);
   const [socket, setSocket] = useState(null);
   const wrapperRef = useRef(null);
-
+//https://coauthor-vbvro8zi.b4a.run
   useEffect(() => {
-    const conn = io("https://coauthor-vbvro8zi.b4a.run", options);
+    const conn = io("http://localhost:8000", options);
     setSocket(conn);
     return () => {
       conn.disconnect();
@@ -40,10 +40,12 @@ const TextEditor = () => {
 
   useEffect(() => {
     if (socket === null || quill === null) return;
-    
+
     socket.on("load-document", (document) => {
+      console.log('load document event')
       quill.setContents(document);
       quill.enable();
+
     });
 
     socket.emit("get-document", documentId);
@@ -53,9 +55,9 @@ const TextEditor = () => {
     if (socket == null || quill == null) return;
     const handler = (delta, oldDelta, source) => {
       if (source !== "user") return;
-      
+
       socket.emit("send-changes", delta);
-      
+
     };
     quill.on("text-change", handler);
 
@@ -67,7 +69,7 @@ const TextEditor = () => {
   useEffect(() => {
     if (socket == null || quill == null) return;
     const interval = setInterval(() => {
-      
+
       socket.emit("save-document", quill.getContents());
     }, SAVE_INTERVAL_MS);
 
@@ -99,12 +101,58 @@ const TextEditor = () => {
       theme: "snow",
       modules: { toolbar: TOOLBAR_OPTIONS },
     });
+    // EditorInstance.clipboard.addMatcher(Node.ELEMENT_NODE, (node, delta) => {
+    //   delta.ops = delta.ops.map(op => {
+    //     return {
+    //       insert: op.insert
+    //     }
+    //   })
+    //   return delta
+    // })
+
     EditorInstance.disable();
     EditorInstance.setText("Loading...");
     setQuill(EditorInstance);
   }, []);
 
-  return <div className="container" ref={wrapperRef}></div>;
+  return (
+    <>
+      <div className="container outerDiv" ref={wrapperRef}></div>
+      {/* <div
+        style={{
+          height: "550px",
+          width: "350px",
+          gap: "10px",
+          position: "absolute",
+          top: "100px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      > */}
+        {/* <video
+          style={{ border: "2px solid black", objectFit: 'cover' }}
+          src={video}
+          height={225}
+          width={340}
+          autoPlay
+          muted
+          controls
+        /> */}
+
+        {/* <video
+          style={{ border: "2px solid black", objectFit: "cover" }}
+          src={video}
+          height={225}
+          width={340}
+          autoPlay
+          muted
+          controls
+        />
+      </div> */}
+    </>
+  );
 };
 
 export default TextEditor;
